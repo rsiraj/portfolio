664bcfb04a24076f8a19c620ede4029309fcd82b
Rehan Siraj <rsiraj@r2hstudent.org>
2022-04-17 14:54:20 -0400

    working on experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import ME from '../../assets/me-about.jpg';
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>300+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>50+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta saepe numquam corporis possimus itaque laborum magni repellat ipsa? Sint voluptate ab et eveniet, quisquam illo reiciendis optio officiis tempora? Voluptas! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error hic iure earum quibusdam non.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk!</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,78 @@
 import React from 'react';
+import {BsPatchCheckFill} from 'react-icons/bs';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__child">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>SASS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>ReactJS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__child">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>NodeJS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>MySQL</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>MongoDB</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
