053ffb78ca55d2d67d4762d735d59762461ce6cb
Rehan Siraj <rsiraj@r2hstudent.org>
2022-04-17 23:40:39 -0400

    done with site skeleton

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  return (
-    <section id='contact'>Contact</section>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_zjf7g3i",
+      "template_ml3fwko",
+      form.current,
+      "mdX5XscbJIeluM9SR"
+    );
+    alert("Thank you for submitting a message.");
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>fake@email.com</h5>
+            <a href="mailto:rehansiraj+site@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>fake@email.com</h5>
+            <a href="mailto:rehansiraj+site@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>fake@email.com</h5>
+            <a href="mailto:rehansiraj+site@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="example@email.com"
+            required
+          />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your message..."
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
